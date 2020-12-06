c0aa13eaab6950bb2a5a8447d47a2eb37c9baf55
Jesse Dodde <jesse@schoolmaster.nl>
2020-12-06 12:28:39 +0100

    Day 6 finished

---
+++ AOC-2020.Ex6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AOC_2020.Ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = System.IO.File.ReadAllText("input.txt").Split("\r\n\r\n");
+
+            Console.WriteLine(input.Sum(s => Regex.Matches(s, @"\w").Select(m => m.Value).Distinct().Count()));
+
+            Console.WriteLine(input.Sum(t => GetConsensusCount(t.Split("\r\n"))));
+        }
+
+        private static int GetConsensusCount(IEnumerable<string> input)
+        {
+            IEnumerable<char> result = null;
+
+            foreach (var item in input)
+            {
+                result = result == null ? item : result.Intersect(item);
+            }
+
+            return result.Count();
+        }
+    }
+}
